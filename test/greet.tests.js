ad0ac9c529a84b36dd87b9bb6681ee0490f3384d
lindokuhlentshangase <163897639+lindokuhlentshangase@users.noreply.github.com>
2024-07-14 11:46:47 +0200

    Update greet.tests.js

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,17 +1,20 @@
-
-
 describe('this is my greet function' , function(){
     it("it should return 'Hello, Zano' when called with Zano", function(){
-        assert.equal(2,2, greet('Mthandazo'), 'this should not be true');
+        assert.equal(2,2,"Hello, Zano",greet("Zano"), "this should not be true");
 
     });
 
     it("it should return 'Hello, Sizwe' when called with Sizwe", function(){
-        assert.equal(2,2, greet('Sizwe', greet('Sizwe')), 'this should not be true');
+        assert.equal(2,2,"Hello, Sizwe",greet("Sizwe"), "this should not be true");
     
     });
 
 });
+
+//console.log(greet("Zano"));
+//console.log(greet("Sizwe"));
+
+
 
 
 
